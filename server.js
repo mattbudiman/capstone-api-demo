3f0663f0b7eee7b745a3ae7f6ffdedff2c5eb748
narnix17 <sabrinaxie11@gmail.com>
2020-11-25 00:58:58 -0700

    fixed getDepartmentsManaged query

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+process.on('SIGINT', function() {
+    console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+    
+    process.exit(1);
+  });
